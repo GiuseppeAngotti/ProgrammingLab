b0458b0cb7bf528dff2e26da617dcb9a1b7f06c3
GiuseAngo <94190650+GiuseppeAngotti@users.noreply.github.com>
2022-11-26 15:50:19 +0000

    provo

---
+++ o.py
@@ -0,0 +1,20 @@
+def sum_csv(nome_file):
+    somma=0.0 
+    file=open(nome_file,'r') 
+    for line in file:
+        elements=line.split(',')
+        if elements[0]=='Date':
+            continue
+        else:
+            try:    
+                somma=somma+float(elements[1])
+            except ValueError:
+                return None
+    if(somma==0.0):
+        return None
+    return somma
+
+
+risultato=sum_csv('shampoo_sales.csv')
+
+print('La somma degli elementi è uguale a: {}'.format(risultato))
