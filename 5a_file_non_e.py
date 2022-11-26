7d3bd0fac6b72213ed9494a884e4272007b4f987
GiuseAngo <94190650+GiuseppeAngotti@users.noreply.github.com>
2022-11-26 18:17:59 +0000

    es 5a

---
+++ 5a_file_non_e.py
@@ -0,0 +1,19 @@
+class CSVFile():
+    def __init__(self,name):
+        self.name=name
+
+    def get_data(self):
+        list_list=[]
+        try:
+            file=open(self.name,'r')
+            for line in file:
+                elements=line.strip('\n').split(',')
+                if elements[0]!='Date':
+                    list_list.append(elements)
+            return list_list
+        except:
+            print('Errore il file che stavi cercando non esiste')
+            return None
+#file_csv= CSVFile('shampoo_sales.csv')
+
+#print('{}'.format(file_csv.get_data()))
