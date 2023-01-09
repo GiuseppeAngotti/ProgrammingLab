5d350c2cd0567bd2ac03755b162142463ecae987
GiuseAngo <94190650+GiuseppeAngotti@users.noreply.github.com>
2023-01-09 15:54:36 +0000

    controllo tipo input (deve essere una lista >1)

--- 8_modelli_fit_predict.py
+++ 8_modelli_fit_predict.py
@@ -12,13 +12,17 @@ class Model():
 #la funzione predict()
 class IncrementModel(Model):
     def predict(self,data):
+        if type(data)!=list:
+            raise TypeError('Il tipo di dati inserito non va bene')
+        if len(data)<=1:
+            raise Exception('Errore non posso prevedere dei dati avendo solo un valore')
         succ=None
         sum_suc=0
         prediction=None
         
         #il valore corrente (t) non deve essere contato:
         #se osservo l'esempio n=3 ma divide per 2
-        i=-1
+        i=0
         val_prec=None
         for item in data:
             if(val_prec!=None):
@@ -27,10 +31,7 @@ class IncrementModel(Model):
             val_prec=item
             i+=1
 
-        if i==0:
-            prediction=val_prec
             #raise Exception('Errore non posso fare una divisione per 0')
-        else:
-            prediction=val_prec+int(sum_suc/i)
+        prediction=val_prec+int(sum_suc/i)
             
         return prediction
