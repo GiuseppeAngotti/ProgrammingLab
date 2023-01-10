1f3c2e585b236ca98beba0afabe0b9a4132bb368
GiuseAngo <94190650+GiuseppeAngotti@users.noreply.github.com>
2023-01-10 12:51:15 +0000

    ESERCIZIO FATTO

--- 8_modelli_fit_predict.py
+++ 8_modelli_fit_predict.py
@@ -1,35 +1,43 @@
 class Model():
- 
-    def fit(self,data):
-        #Fit non implementato nella classe base
-        raise NotImplementedError('Metodo non implementato')
- 
-    def predict(self,data):
-        #Predict non implementato nella classe base
-        raise NotImplementedError('Metodo non implementato')
- 
-#creo un oggetto IncrementModel che estende Model e implemento 
+ 
+    def fit(self,data):
+        #Fit non implementato nella classe base
+        raise NotImplementedError('Metodo non implementato')
+ 
+    def predict(self,data):
+        #Predict non implementato nella classe base
+        raise NotImplementedError('Metodo non implementato')
+ 
+#creo un oggetto IncrementModel che estende Model e implemento 
 #la funzione predict()
 class IncrementModel(Model):
-    def predict(self,data):
-        if type(data) is not list:
-            raise Exception("Errore l'input inserito non è una lista")
-        if len(data)==1:
-            raise Exception('Errore non posso fare una divisione per 0')
-        succ=None
-        sum_suc=0
-        prediction=None
-        #il valore corrente (t) non deve essere contato:
-        #se osservo l'esempio n=3 ma divide per 2
-        i=-1
-        val_prec=None
-        for item in data:
-            if type(item)!=float or type(item)!=int:
-                raise Exception('Errore non posso lavorare con valori non numerici')
-            if(val_prec!=None):
-                succ=item-val_prec
-                sum_suc+=succ
-            val_prec=item
-            i+=1
-        prediction=val_prec+int(sum_suc/i)
-        return prediction
+    def predict(self,data):
+        if type(data) is not list:
+            raise Exception("Errore l'input inserito non è una lista")
+        if len(data)==1:
+            raise Exception('Errore non posso fare una divisione per 0')
+        succ=None
+        sum_suc=0
+        prediction=None
+        #il valore corrente (t) non deve essere contato:
+        #se osservo l'esempio n=3 ma divide per 2
+        i=-1
+        val_prec=None
+        for item in data:
+            if(val_prec!=None):
+                succ=item-val_prec
+                sum_suc+=succ
+            val_prec=item
+            i+=1
+        prediction=val_prec+int(sum_suc/i)
+        return prediction
+        
+##per poter fare le PROVE
+#values=[50,52,60]
+
+##questo è un dizionario e non va bene 
+#values={'1':1}
+
+##istanzio l'oggetto 'modello'
+#increment_model=IncrementModel()
+#print(increment_model.predict(values))
