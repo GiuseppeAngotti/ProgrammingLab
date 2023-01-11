3a8908c0a0ef34730e0e53fd2103f51a725c6461
GiuseAngo <94190650+GiuseppeAngotti@users.noreply.github.com>
2023-01-11 18:53:01 +0000

    prova 4.1 per il 10

--- 8_modelli_fit_predict.py
+++ 8_modelli_fit_predict.py
@@ -38,9 +38,9 @@ class IncrementModel(Model):
         return prediction
         
 ##per poter fare le PROVE
-values=[50,52,60]
+#values=[50,52,60]
 ##questo è un dizionario e non va bene 
 #values={'1':1}
 ##istanzio l'oggetto 'modello'
-increment_model=IncrementModel()
-print(increment_model.predict(values))
+#increment_model=IncrementModel()
+#print(increment_model.predict(values))
