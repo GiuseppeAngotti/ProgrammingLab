e8d1d3087c509b6e4fee641bc696d21badfd41b5
GiuseppeAngotti <94190650+GiuseppeAngotti@users.noreply.github.com>
2023-01-11 18:50:48 +0100

    Update esame.py

--- esame.py
+++ esame.py
@@ -37,6 +37,6 @@ class MovingAverage():
         
         return average_list
 
-moving_average = MovingAverage(2)
+moving_average = MovingAverage(5)
 result = moving_average.compute([2,4,8,16,32])
 print(result) # Deve stampare a schermo [3,6,12]
